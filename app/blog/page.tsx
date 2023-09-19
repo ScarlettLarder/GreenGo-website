bb48b06d72ce93e2a45c3cbe1e3d2abb52819aa9
luke <lseward10@gmail.com>
2023-09-19 13:05:12 +0100

    blog frontend done

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 /**  ---Quick start guide for web dev, delete if/when not needed--- 
  * 
  * TSX is both javascript and html. Html is used after return in tsx, while javascript can be added before the return, where const wow is. 
@@ -15,11 +17,47 @@
  * (oh and "npm run dev" in the terminal to see your site btw, and use testbranch for saving)
  */
 
+
 export default function blog() {
 
     const wow = "insane";
 
     return(
-        <p className="pt-32 bg-red-100">blog</p>
+        <main>
+            <div className="mt-24 ml-16">
+                <h1 className="text-5xl">Blog Page</h1>
+                <p className="text-3xl text-gray-600">The latest info from Green Go, right here</p>
+            </div>
+            <div className="grid grid-cols-1 justify-center gap-12 mx-16 my-20 ">
+
+                <div className="rounded-lg bg-lightgreen flex justify-between drop-shadow-2xl">
+                    <div className="p-10">
+                        <h1 className="text-5xl ">Green Go, went? </h1>
+                        <p className="pt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...</p>
+                    </div>
+                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10' src='/Main_Forest.jpg' alt="Forest" width={400} height={400}/>
+                </div>
+
+                <div className="bg-backgreen rounded-lg flex justify-between drop-shadow-2xl">
+                    <div className="p-10">
+                        <h1 className="text-5xl ">Green Go, went? </h1>
+                        <p className="pt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...</p>
+                    </div>
+                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10' src='/Main_Health.jpg' alt="Health" width={350} height={350}/>
+                </div>
+
+                <div className="bg-lightgreen rounded-lg flex justify-between drop-shadow-2xl" >
+                    <div className="p-10">
+                        <h1 className="text-5xl ">Green Go, went? </h1>
+                        <p className="pt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...</p>
+                    </div>
+                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10' src='/Main_Ingredients.jpg' alt="Ingredients" width={400} height={400}/>
+
+                </div>
+            </div>
+        </main>
     )
 }
