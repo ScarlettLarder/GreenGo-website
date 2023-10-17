5164b4b48a7f6a2ccab11b76110c3897bc345121
luke <lseward10@gmail.com>
2023-10-17 21:43:48 +0100

    xoxo

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,40 +6,40 @@ export default function blog() {
 
     return(
         <main>
-            <div className="mt-24 ml-16">
+            <div className="md:mt-24 md:ml-16 mt-12 ml-8">
                 <h1 className="text-6xl underline decoration-greengo">Blog Page</h1>
                 <p className="text-3xl text-gray-600">The latest info from Green Go, right here</p>
             </div>
-            <div className="grid grid-cols-1 justify-center gap-12 my-20 sm:mx-4">
+            <div className="grid grid-cols-1 justify-center gap-12 my-20 ">
 
-                <div className="rounded-lg bg-lightgreen flex justify-between drop-shadow-2xl">
+                <div className="rounded-lg bg-lightgreen flex justify-between drop-shadow-2xl mx-4 md:mx-16">
                     <div className="p-10">
                         <h1 className="text-5xl ">Green Go, went? </h1>
                         <p className="pt-5 text-3xl text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...</p>
                         <p className="pt-5 text-2xl text-gray-600">By big blogger</p>
                     </div>
-                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10 hidden md:visible' src='/Main_Forest.jpg' alt="Forest" width={400} height={400}/>
+                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10 hidden md:flex' src='/Main_Forest.jpg' alt="Forest" width={400} height={400}/>
                 </div>
 
-                <div className="bg-backgreen rounded-lg flex justify-between drop-shadow-2xl">
+                <div className="bg-backgreen rounded-lg flex justify-between drop-shadow-2xl mx-4 md:mx-16">
                     <div className="p-10">
                         <h1 className="text-5xl ">Green Go, went? </h1>
                         <p className="pt-5 text-3xl text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...</p>
                         <p className="pt-5 text-2xl text-gray-600">By big blogger</p>
                     </div>
-                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10' src='/Main_Health.jpg' alt="Health" width={350} height={350}/>
+                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10 hidden md:flex' src='/Main_Health.jpg' alt="Health" width={350} height={350}/>
                 </div>
 
-                <div className="bg-lightgreen rounded-lg flex justify-between drop-shadow-2xl" >
+                <div className="bg-lightgreen rounded-lg flex justify-between drop-shadow-2xl mx-4 md:mx-16" >
                     <div className="p-10">
                         <h1 className="text-5xl ">Green Go, went? </h1>
                         <p className="pt-5 text-3xl text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...</p>
                         <p className="pt-5 text-2xl text-gray-600">By big blogger</p>
                     </div>
-                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10' src='/Main_Ingredients.jpg' alt="Ingredients" width={400} height={400}/>
+                    <Image className='rounded-tl-xl rounded-tr-3xl rounded-b-3xl right-0 m-10 hidden md:flex' src='/Main_Ingredients.jpg' alt="Ingredients" width={400} height={400}/>
                     
                 </div>
             </div>
