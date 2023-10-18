31f1495880bf34e431274f5fb11c1fb50c15a014
lost <skekmicro@gmail.com>
2023-10-18 11:26:39 +0100

    BRUH

--- app/sustainability/page.tsx
+++ app/sustainability/page.tsx
@@ -1,25 +1,131 @@
-/**  ---Quick start guide for web dev, delete if/when not needed--- 
- * 
- * TSX is both javascript and html. Html is used after return in tsx, while javascript can be added before the return, where const wow is. 
- * You can also create functions outside of the default function if needed. Raw CSS can be implimented in the "global.css", but you should try to use
- * tailwind most of the time by adding ClassName to your headings, like in the <p></p> tag and adding varibles like the ones in the cheatsheet (https://tailwindcomponents.com/cheatsheet/)
- * Tailwind is a library for css that makes it easier to work with; please look at the Core Concepts pages in the docs:
- * https://tailwindcss.com/docs/utility-first
- * Tailwind CheatSheet can be used to quickly get class names, such as sizes, colours ect. Just look it up in the search bar!
- * https://tailwindcomponents.com/cheatsheet/
- * 
- * For some general tips, try to make your site to look like the figma design as much as possible, tailwind.config.ts has preset colours from the pallet that you can call
- * instead of "red-100" for example. Look at the base page.tsx for main page if you want to look at some of my code to use for reverse engineering.
- * 
- * Goodluck!
- * (oh and "npm run dev" in the terminal to see your site btw, and use testbranch for saving)
- */
+import { FunctionComponent } from "react";
 
-export default function Sustainability() {
+const Sustainability: FunctionComponent = () => {
+  return (
+    <div className="relative [background:linear-gradient(219.57deg,_#92d0ff,_#b2ffb9_46.59%,_#ffdfff),_#fff] w-full h-[2368px] overflow-hidden text-center text-3xl text-black font-just-me-again-down-here">
+      <div className="absolute top-[116px] left-[53px] w-[1271px] h-[1680px] overflow-hidden">
+        <div className="absolute top-[1321px] left-[46px] w-[1194px] h-[359px]">
+          <img
+            className="absolute top-[40px] left-[623px] rounded-xl w-[575px] h-[319px] object-cover"
+            alt=""
+            src="/image@2x.png"
+          />
+          <div className="absolute top-[0px] left-[0px] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[436.46px] h-[308px]">
+            <div className="absolute top-[0px] left-[352px] w-[73px] h-[295.28px] overflow-hidden">
+              <img
+                className="absolute top-[5.78px] left-[5.78px] rounded-14xl-5 w-[67.22px] h-[68.16px] object-cover"
+                alt=""
+                src="/plant-symbolremovebgpreview-1@2x.png"
+              />
+            </div>
+            <div className="absolute top-[66px] left-[0px] rounded-xl [background:linear-gradient(180deg,_#fff,_#ffbcff)] w-[436px] h-[242px] overflow-hidden" />
+            <div className="absolute top-[110px] left-[141px] w-[154px] h-[198px] overflow-hidden">
+              <img
+                className="absolute top-[0px] left-[0px] w-[154px] h-[154px] object-cover"
+                alt=""
+                src="/images--1-removebgpreview-1@2x.png"
+              />
+            </div>
+            <div className="absolute top-[66px] left-[18px] w-[399px] h-[242px] overflow-hidden">
+              <div className="absolute top-[0px] left-[0px] flex items-center justify-center w-[399px] h-[242px]">
+                From the abattoir to your hands, we ensure that every step of our
+                production process is ethically sound. Our factories prioritize
+                renewable energy sources, waste reduction, and fair labor
+                practices. We are committed not just to energize you, but to
+                invigorate our world with positive and sustainable actions.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[705px] left-[0px] w-[1247px] h-[579px]">
+          <div className="absolute top-[137px] left-[811px] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[436px] h-[305.36px]">
+            <img
+              className="absolute top-[2.52px] left-[10.19px] rounded-14xl-5 w-[72.99px] h-[67.47px] object-cover"
+              alt=""
+              src="/plant-symbolremovebgpreview-2@2x.png"
+            />
+            <div className="absolute top-[63.36px] left-[0px] rounded-xl [background:linear-gradient(180deg,_#fff,_#f8cce2)] w-[436px] h-[242px]" />
+            <img
+              className="absolute top-[98.36px] left-[140px] w-[154px] h-[154px] object-cover"
+              alt=""
+              src="/images--1-removebgpreview-2@2x.png"
+            />
+            <div className="absolute top-[63.36px] left-[18px] flex items-center justify-center w-[399px] h-[242px]">
+              Our planet's health is paramount. Our energy drink packaging
+              reflects this ethos. We utilize renewable, recyclable, and
+              biodegradable materials to reduce our environmental footprint.
+              Every can you hold has been designed with Mother Earth in mind,
+              ensuring that while you recharge, our planet doesn't deplete.
+            </div>
+          </div>
+          <img
+            className="absolute top-[0px] left-[0px] w-[583px] h-[588px] object-cover"
+            alt=""
+            src="/img-0587edit-1@2x.png"
+          />
+        </div>
+        <div className="absolute top-[215px] left-[0px] w-[1271px] h-[490px] text-[24px]">
+          <div className="absolute top-[1px] left-[0px] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[526.08px] h-[358px]">
+            <img
+              className="absolute top-[4.22px] left-[387.31px] rounded-14xl-5 w-[72.95px] h-[67.82px] object-cover"
+              alt=""
+              src="/plant-symbolremovebgpreview-3@2x.png"
+            />
+            <div className="absolute top-[66px] left-[0px] rounded-xl [background:linear-gradient(180deg,_#fff,_#ffd1ff)] w-[526.08px] h-[292px]" />
+            <img
+              className="absolute top-[135px] left-[185.08px] w-[154px] h-[154px] object-cover"
+              alt=""
+              src="/images--1-removebgpreview-3@2x.png"
+            />
+            <div className="absolute top-[66px] left-[21.72px] leading-[166.8%] flex items-center justify-center w-[481.44px] h-[292px]">
+              We believe in the purity of nature and the power of responsible
+              sourcing. Our energy drinks are crafted using only the finest
+              ingredients, procured from organic and sustainable farms. This not
+              only ensures a high-quality product for our consumers but also
+              champions local farmers and eco-friendly farming practices.
+            </div>
+          </div>
+          <div className="absolute top-[0px] left-[740px] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[531px] h-[490px]">
+            <img
+              className="absolute top-[224px] left-[0px] w-[531px] h-[266px] object-cover"
+              alt=""
+              src="/istockphoto1193959759612x612removebgpreview-1@2x.png"
+            />
+            <img
+              className="absolute top-[0px] left-[0px] rounded-xl w-[531px] h-[352px] object-cover"
+              alt=""
+              src="/6615939-a5e6f792-1@2x.png"
+            />
+          </div>
+        </div>
+        <i className="absolute top-[126px] left-[227px] text-[13px] leading-[166.8%] flex font-thin font-inter text-left items-center w-[387px] h-[88px]">
+          “Our mission was to make an energy powered by nature for nature.
+          Therefore, our drink has been made as sustainable as possible. Here is
+          how we’ve done that..” - Scarlett
+        </i>
+        <img
+          className="absolute top-[0px] left-[137px] w-[1085.5px] h-[95.45px] object-cover"
+          alt=""
+          src="/title@2x.png"
+        />
+      </div>
+      <div className="absolute top-[116px] left-[0px] w-[3833px] h-[2356px] overflow-hidden">
+        <div className="absolute top-[2355.88px] left-[0px] w-[3833px] h-[0.12px] overflow-hidden">
+          <div className="absolute top-[0px] left-[0px] w-[3833px] h-[0.12px] overflow-hidden">
+            <div className="absolute top-[0px] left-[0px] w-[3833px] h-[0.12px] overflow-hidden">
+              <div className="absolute top-[0px] left-[0px] w-[3833px] h-[0.12px] overflow-hidden">
+                <img
+                  className="absolute top-[0px] left-[3851.61px] w-[3833px] h-[1539px] object-cover"
+                  alt=""
+                  src="/istockphoto1193959759612x612removebgpreview-2@2x.png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    const wow = "insane";
-
-    return(
-        <p className="pt-32 bg-red-100">sustainability</p>
-    )
-}
+export default Sustainability;
